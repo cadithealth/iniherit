2028d677c44e7b3cf29e1ae8fce4f58ffbe65e04
grabner <pjg.github@ubergrabner.net>
2013-08-26 17:19:50 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ entrypoints = {
 
 setup(
   name                  = 'iniherit',
-  version               = '0.1.5',
+  version               = '0.1.6',
   description           = 'A ConfigParser subclass with file-specified inheritance.',
   long_description      = read('README.rst'),
   classifiers           = [
